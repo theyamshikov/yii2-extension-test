695e22bcc24ccdfe2989a01b73082c77a8331c7e
Vitaliy <theyamshikov@yandex.ru>
2017-06-21 11:49:18 +0600

    fix and clean

--- src/RestCest.php
+++ src/RestCest.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace woop\test;
+namespace woop\extension\test;
 
 use yii\helpers\ArrayHelper;
 
 class RestCest
 {
-	/* @var $I FunctionalTester */
 	protected $authId = null;
 	
 	const TYPE_DATE = 'string:date|null';
@@ -19,36 +18,13 @@ class RestCest
 	const TYPE_ARRAY = 'array';
 	const TYPE_ARRAY_OR_NULL = 'array|null';
 	
-	public function _before(FunctionalTester $I) {
+	public function _before($I) {
 		$I->haveHttpHeader('language', 'xx');
 		if(method_exists($this, '_fixtures')) {
 			$fixtures = $this->_fixtures();
 			$I->haveFixtures($fixtures);
 		}
-		//$I->auth($this->authId);
 		$I->format = $this->format;
 	}
-	
-	/*protected function checkAccessAllowed($userList = null, $uri = null) {
-		$uri = $uri ? $uri : $this->uri;
-		foreach($userList as $user) {
-			$this->auth($user);
-			$this->I->sendGET($uri);
-			$this->I->SeeResponseCodeIs(200);
-		}
-	}
-	
-	protected function checkAccessDenied($userList = null, $uri = null) {
-		$uri = $uri ? $uri : $this->uri;
-		foreach($userList as $user) {
-			$token = $this->auth($user);
-			$this->I->sendGET($uri);
-			if($token) {
-				$this->I->SeeResponseCodeIs(403);
-			} else {
-				$this->I->SeeResponseCodeIs(401);
-			}
-		}
-	}*/
 	
 }
