2148cdd4d584ed34f04ca8ecdd1570e5c4df5e6f
Vitaliy <theyamshikov@yandex.ru>
2017-07-29 11:23:34 +0600

    create type enum

--- src/RestCest.php
+++ src/RestCest.php
@@ -2,23 +2,10 @@
 
 namespace yii2lab\test;
 
-use yii\helpers\ArrayHelper;
-
 class RestCest
 {
 	protected $authId = null;
 	public $format = [];
-	
-	const TYPE_DATE = 'string:date|null';
-	const TYPE_FLOAT = 'float|integer';
-	const TYPE_STRING = 'string';
-	const TYPE_STRING_OR_NULL = 'string|null';
-	const TYPE_INTEGER = 'integer';
-	const TYPE_INTEGER_OR_NULL = 'integer|null';
-	const TYPE_BOOLEAN = 'boolean';
-	const TYPE_BOOLEAN_OR_NULL = 'boolean|null';
-	const TYPE_ARRAY = 'array';
-	const TYPE_ARRAY_OR_NULL = 'array|null';
 
 	const EMPTY_STRING = '';
 	
@@ -28,7 +15,7 @@ class RestCest
 			$fixtures = $this->_fixtures();
 			$I->haveFixtures($fixtures);
 		}
-		$I->format = $this->format;
+		$I->format = $this->format; // todo: kill
 	}
 
 	protected function loadFixtures($fixtures) {
