2e9607e512f29d283fd8b599d199011f536e0bb8
Vitaliy <theyamshikov@yandex.ru>
2018-01-19 10:53:50 +0600

    make UnitAssertTrait

---
+++ src/traits/UnitAssertTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace yii2lab\test\traits;
+
+use yii2lab\domain\BaseEntity;
+
+trait UnitAssertTrait
+{
+	
+	public function assertEntity(array $expect, BaseEntity $entity) {
+		$this->assertArraySubset($expect, $entity->toArray());
+	}
+	
+	public function assertCollection(array $expect, array $collection) {
+		expect(true)->equals(count($expect) == count($collection));
+		$expect = array_values($expect);
+		$collection = array_values($collection);
+		foreach($collection as $key => $entity) {
+			$this->assertEntity($expect[$key], $entity);
+		}
+	}
+	
+	public function assertIsCollection(array $collection) {
+		foreach($collection as $entity) {
+			expect(true)->equals($entity instanceof BaseEntity);
+		}
+	}
+	
+}
