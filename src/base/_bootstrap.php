bafe489e08b7c755d49d46b36ebef5fdfeccc002
Vitaliy <theyamshikov@yandex.ru>
2018-01-21 01:21:50 +0600

    fix

--- src/base/_bootstrap.php
+++ src/base/_bootstrap.php
@@ -3,7 +3,7 @@
 use yii2lab\app\App;
 
 $name = 'console';
-define('YII_ENV', 'test');
+defined('YII_ENV') OR define('YII_ENV', 'test');
 
 require_once(realpath(__DIR__ . '/../../../../yii2lab/yii2-app/src/App.php'));
 
