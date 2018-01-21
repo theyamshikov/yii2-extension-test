5866ae48e4d4079b4f79270b189a9d0705ef48a1
Vitaliy <theyamshikov@yandex.ru>
2018-01-21 20:24:32 +0600

    update test

--- src/base/_bootstrap.php
+++ src/base/_bootstrap.php
@@ -7,4 +7,4 @@ defined('YII_ENV') OR define('YII_ENV', 'test');
 
 require_once(realpath(__DIR__ . '/../../../../yii2lab/yii2-app/src/App.php'));
 
-App::init($name, 'vendor/yii2lab/yii2-app/tests/store/app/common/config');
+App::init($name, 'vendor/yii2lab/yii2-app/tests/store/app');
