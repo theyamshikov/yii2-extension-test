c5323fa7d23981a0ae2602af07f263dd903e3e11
amubarak@wooppay.kz <amubarak@wooppay.kz>
2019-11-05 16:53:03 +0600

    fix test

--- src/base/_config/main.php
+++ src/base/_config/main.php
@@ -1,12 +1,8 @@
 <?php
 
 use yii2lab\app\domain\helpers\Config;
-use yii2lab\app\domain\helpers\Env;
 
+$config = env('config');
 
-//if($this->getParameter('test_all')){
-//	$config=[];
-//} else{
-Env::init('vendor/yii2lab/yii2-test/src/base/_application');
-$config = Env::get('config');
 return Config::loadData($config);
+
