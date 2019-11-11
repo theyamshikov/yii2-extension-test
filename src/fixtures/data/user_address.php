d5449099af80d4a38f42a194df9625f3d0d6c858
amubarak@wooppay.kz <amubarak@wooppay.kz>
2019-11-11 16:13:26 +0600

    fix test

--- src/fixtures/data/user_address.php
+++ src/fixtures/data/user_address.php
@@ -1,3 +1,3 @@
 <?php
 
-return include(ROOT_DIR . '/vendor/yii2module/yii2-account/src/domain/v2/fixtures/data/user-address.php');
+return include(ROOT_DIR . '/vendor/yii2module/yii2-account/src/domain/v2/fixtures/data/user_address.php');
