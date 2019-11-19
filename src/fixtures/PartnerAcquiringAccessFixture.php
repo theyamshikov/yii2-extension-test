448230b23e572f207d2754d2c5e252ce6ce31b62
amubarak@wooppay.kz <amubarak@wooppay.kz>
2019-11-19 15:29:30 +0600

    add partner_acquiring_access fixtures

---
+++ src/fixtures/PartnerAcquiringAccessFixture.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace common\fixtures;
+
+use yii\test\ActiveFixture;
+
+class PartnerAcquiringAccessFixture extends ActiveFixture
+{
+	public $tableName = '{{%partner_acquiring_access}}';
+}
---
+++ src/fixtures/data/partner_acquiring_access.php
@@ -0,0 +1,124 @@
+<?php
+
+return [
+	[
+		'id' => 1,
+		'partner_id' => 2,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 2,
+		'partner_id' => 4,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 3,
+		'partner_id' => 6,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 4,
+		'partner_id' => 8,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 5,
+		'partner_id' => 11,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 6,
+		'partner_id' => 12,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 7,
+		'partner_id' => 16,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 8,
+		'partner_id' => 14,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 9,
+		'partner_id' => 7,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 10,
+		'partner_id' => 13,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 11,
+		'partner_id' => 25,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 12,
+		'partner_id' => 26,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 13,
+		'partner_id' => 9,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 14,
+		'partner_id' => 29,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 15,
+		'partner_id' => 3,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 16,
+		'partner_id' => 5,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 17,
+		'partner_id' => 1,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 18,
+		'partner_id' => 17,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 19,
+		'partner_id' => 10,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+	[
+		'id' => 20,
+		'partner_id' => 31,
+		'service_id' => null,
+		'terminal_id' => 1,
+	],
+];
