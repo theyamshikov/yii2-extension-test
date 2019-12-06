cc72c514d74fddb485f185deaa113490e31e6814
amubarak@wooppay.kz <amubarak@wooppay.kz>
2019-12-06 09:53:54 +0600

    service fixture update

--- src/fixtures/data/service_blacklist.php
+++ src/fixtures/data/service_blacklist.php
@@ -3,22 +3,22 @@
 return [
 	[
 		'item_type' => 'category',
-		'item_id' => '14',
+		'item_id' => 14,
 	],
 	[
 		'item_type' => 'category',
-		'item_id' => '20',
+		'item_id' => 20,
 	],
 	[
 		'item_type' => 'service',
-		'item_id' => '18',
+		'item_id' => 18,
 	],
 	[
 		'item_type' => 'service',
-		'item_id' => '28',
+		'item_id' => 28,
 	],
 	[
 		'item_type' => 'field',
-		'item_id' => '2884999',
+		'item_id' => 2884999,
 	],
 ];
--- src/fixtures/data/service_locale.php
+++ src/fixtures/data/service_locale.php
@@ -2,13 +2,13 @@
 
 return [
 	[
-		'service_id' => '19',
-		'city_id' => '1',
-		'country_id' => '1',
+		'service_id' => 19,
+		'city_id' => 1,
+		'country_id' => 1,
 	],
 	[
-		'service_id' => '2',
-		'city_id' => '1',
-		'country_id' => '1',
+		'service_id' => 2,
+		'city_id' => 1,
+		'country_id' => 1,
 	],
 ];
